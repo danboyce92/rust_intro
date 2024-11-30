78e54a0c6a8f9872cba0bcf4af17c0b34fcf6bbb
Daniel B <daarf92@gmail.com>
2024-11-30 12:30:05 +0000

    Results & matching example

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+use std::io::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let text = fs::read_to_string("logs.txt");
+
+    // println!("{:#?}", text);
+    match divide(5.0, 2.0) {
+        Ok(results_of_division) => {
+            println!("{}", results_of_division)
+        } 
+        Err(what_went_wrong) => {
+            println!("{}", what_went_wrong);
+        }
+    }
+
+}
+
+
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        Err(Error::other("can't divide by 0"))
+    } else {
+        Ok(a / b)
+    }
 }
